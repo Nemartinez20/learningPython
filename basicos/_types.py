ec1fa5102348f1857f88a27be34ba559211d2bb7
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-09 20:11:27 -0500

    Se agrega tipos de datos en python

---
+++ basicos/_types.py
@@ -0,0 +1,34 @@
+#types
+
+# int ,float, complejos, strings, bool, noType, list, tuplas, arrays, diccionarios, range, set
+
+# Tipo de numero enteros
+print('int:=======================')
+print(1)
+print(0)
+print(-1)
+print(14324239123791283012)
+
+
+# Saber rl typo de dato
+print(type(32423))
+
+
+"""
+esto es un
+comentario de 
+multilinea
+
+"""
+# Tipo de numero decimales o float
+print('float:=======================')
+print(1.0)
+print(3.1415)
+print(-1.34)
+print(14.324239123791283012)
+
+# Tipo de numero decimales o float
+print('complex:=======================')
+print(1 + 3j)
+print(3.1415)
+
