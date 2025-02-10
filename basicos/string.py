e3758c86a5dbac080ed35c0b32c42343937f7a95
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-09 19:50:07 -0500

    se agrega repaso de print

--- basicos/string.py
+++ basicos/string.py
@@ -1,2 +1,10 @@
 
-print('hola munedo')
+##imprimir en pantalla
+print("Hello, World!")
+## imprimir multiple
+print('hola','como', 'estan', 'Python' , 'es', 'genial')
+##Cambiar el separador
+print('Python','es', 'muy', 'muy' , 'Brutal', sep ='*')
+## imprimir en una sola linea
+print("esto se imprime en", end ="!")
+print("una sola linea" )
