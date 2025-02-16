e77499800893050efb39b3b38933aebe30ebd8e1
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-16 18:53:16 -0500

    agregando funciones

--- 03_Loops/04_Funciones.py
+++ 03_Loops/04_Funciones.py
@@ -29,3 +29,61 @@ def saludar_a(nombre):
 saludar_a('Nestor Arley')
 saludar_a('Mhilan')
 saludar_a('Sara Valentina')
+
+# funciones con mas paramentros
+
+print('mas argumentos \n')
+
+def sumar(a,b):
+    suma = a + b
+    return suma
+
+print(sumar(8,7))
+
+# Documentar las funciones con docstring
+def restar(a,b):
+    """Resta dos numeros y devuelve el resultado"""
+    resta= a-b
+    return resta
+
+print(restar(10,5))
+
+#Argumentos por defecto
+
+def multiplicar(a, b = 10):
+    mlt = a * b 
+    return mlt
+
+print(multiplicar(5))
+
+##Argumentos por posicion
+
+def descibir_person(nombre, edad, sexo):
+    print(f'hola soy {nombre} tengo {edad} años y me identifico como {sexo}')
+
+
+# funcion  con paramentros por posicion. 
+descibir_person('juan',39,'Masculino')
+
+## Argumentos pos valor o clave
+descibir_person( edad = 25, sexo='femenino', nombre='Maria')
+
+
+# Argumentos de longitud variable  (*args)
+
+def sumar_numeros(*args):
+    suma = 0
+    for numero in args:
+        suma = suma + numero
+    return suma 
+
+print(sumar_numeros(1,5,2,4,7))
+print(sumar_numeros(1,5,2,4,7,8,7,9,5,4,6,2,5))
+
+# Argumentos de clave-valor variable (**kwargs):
+
+def saludando_aX(**kwargs):
+    for  clave , valor in kwargs.items():
+        print(f'{clave} : {valor}')
+
+saludando_aX(nombre='juan',edad=30, sexo='Masculino')
