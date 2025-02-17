0fd049a1e4447fea93cb8a3caeef1b9dbe8ed50f
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-16 20:34:26 -0500

    agregando ejercicios de logica

---
+++ 04_Logic/02_challenge_jurassic_park.py
@@ -0,0 +1,22 @@
+"""
+En Jurassic Park, se ha observado que los dinosaurios carnívoros, como el temible T-Rex, depositan un número par de huevos. Imagina que tienes una lista de números enteros en la que cada número representa la cantidad de huevos puestos por un dinosaurio en el parque.
+
+Importante: Solo se consideran los huevos de los dinosaurios carnívoros (T-Rex) aquellos números que son pares.
+
+Objetivo:
+Escribe una función en Python que reciba una lista de números enteros y devuelva la suma total de los huevos que pertenecen a los dinosaurios carnívoros (es decir, la suma de todos los números pares en la lista).
+"""
+
+
+lista = [2,3,4,5,6,7,8,9,10,23,45,23,22,24,26]
+def conatrHuevos(lis):
+    total = 0
+
+    for unalist in lis :
+        if unalist % 2 == 0:
+            print(unalist)
+            total = total + unalist
+    return total
+        
+    
+print(conatrHuevos(lista))
